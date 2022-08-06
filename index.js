6b9127b1a03045b7ca68ebe081c25cbb5fc77cd9
rm17394 <mishrahul17@gmail.com>
2022-08-06 10:39:56 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+var cors = require('cors')
+const connectToMongo=require('./db');
+
+
+connectToMongo();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Deepak!')
+// })
+
+// Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+// hosting on heroku
+if ( process.env.NODE_ENV == "production"){
+
+  app.use(express.static("inotebook/build"));
+}
+
+app.listen(port, () => {
+  console.log(`dailyNotes backend listening at http://localhost:${port}`)
+})
+
+
---
+++ inotebook/src/Components/AddNote.js
@@ -0,0 +1,46 @@
+import React ,{ useContext, useState }from 'react'
+
+import noteContext from "../context/notes/noteContext";
+
+const AddNote = (props) => {
+    const context=useContext(noteContext);
+    const {addNote}=context;
+
+    const [note,setNote]=useState({title:"",description:"",tag:""})
+
+    const handleClick=(e)=>{
+        e.preventDefault();
+        addNote(note.title,note.description,note.tag);
+        setNote({title:"",description:"",tag:""})
+        props.showAlert("Added successfully","success")
+    }
+
+    const onChange=(e)=>{
+       setNote({...note,[e.target.name]:e.target.value})
+    }
+
+  return (
+
+      <div className="container my-3"  style={{color:props.mode==='dark'?'white':'black'}} >
+                <h2>Add a Note</h2>
+                <form className="my-3">
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input type="text" className="form-control" id="title" name="title" value={note.title} aria-describedby="emailHelp" onChange={onChange}  style={{backgroundColor:props.mode==='dark'?'#193e30':'white',color:props.mode==='dark'?'white':'black'}} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Description</label>
+                        <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange}  style={{backgroundColor:props.mode==='dark'?'#193e30':'white',color:props.mode==='dark'?'white':'black'}} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="tag" className="form-label">Tag</label>
+                        <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange}  style={{backgroundColor:props.mode==='dark'?'#193e30':'white',color:props.mode==='dark'?'white':'black'}}  />
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
+                </form>
+            </div>
+        
+  )
+}
+
+export default AddNote
