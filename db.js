5aa7a37574c6886942fd10f700dbc02c0fe8fc04
rm17394 <mishrahul17@gmail.com>
2022-12-22 18:32:31 +0530

    new uri added

--- db.js
+++ db.js
@@ -3,14 +3,22 @@ const dotenv=require("dotenv");
 
 dotenv.config({path:'./config.env'})
 
-const mongoURI=process.env.mongoUrl;
+// const mongoURI=process.env.mongoUrl;
+const newURI = 'mongodb+srv://deepak:deepak@cluster0.eucjtxg.mongodb.net/dailyNotes?retryWrites=true&w=majority'
+// console.log(mongoURI,process.env)
 
 //mongodb://localhost:27017/inotebook
 
+
 const connectToMongo= ()=>{
-    mongoose.connect(mongoURI).then(()=>{
+    mongoose.connect(newURI).then(()=>{
         console.log("Connected to Mongo Atlas Successfully");
-    }).catch((err)=>console.log(err));
+    }).catch((err)=>console.log("Deepak your MongoDB is showing an error  "+err));
 }
 
 module.exports=connectToMongo;
+
+
+// new= /mongodb+srv://deepak:deepak@cluster0.eucjtxg.mongodb.net/dailyNotes?retryWrites=true&w=majority 
+
+// old=/mongodb+srv://book:book@book.ilssjda.mongodb.net/book?retryWrites=true&w=majority 
